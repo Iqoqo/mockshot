33c219d0e4bc3bdab80264fa6f3dc00510dd6b27
Shlomo Kraus <plonit7@gmail.com>
2018-06-30 14:21:54 +0300

    Rename vars

--- src/toMatchMock.ts
+++ src/toMatchMock.ts
@@ -9,15 +9,15 @@ expect.addSnapshotSerializer({
 function getSnapshotName(testName) {
   return `${testName}: [mockshot] 1`;
 }
-function toMatchMock(received, className: string, methodName: string, mockName: string, propertyMatchers: string[]) {
+function toMatchMock(received, className: string, methodName: string, mockName: string, ignoredKeyPaths?: string[]) {
   const snapshotName = getSnapshotName(this.currentTestName);
   const currentSnapshot = this.snapshotState._snapshotData[snapshotName];
   
-  if (propertyMatchers && currentSnapshot) {
+  if (ignoredKeyPaths && currentSnapshot) {
     const parsedSnapshot = JSON.parse(currentSnapshot);
     received = cloneDeep(received);
 
-    propertyMatchers.forEach(keyPath => {
+    ignoredKeyPaths.forEach(keyPath => {
       const val = get(parsedSnapshot.mock, keyPath);
       const target = get(received, keyPath);
       if(val && target && typeof(val) === typeof(target)){
