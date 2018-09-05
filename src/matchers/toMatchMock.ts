4efd68f6f94be60481ffc79a4951eb2745be8cd9
Eli Shvartsman <eli@shvar.com>
2018-09-05 13:26:55 +0300

    cleanup

--- src/matchers/toMatchMock.ts
+++ src/matchers/toMatchMock.ts
@@ -1,6 +1,5 @@
 import { cloneDeep, get, set } from "lodash";
 import pretty from "json-pretty";
-import { generateMocks } from "./generateMocks";
 
 declare global {
   namespace jest {
@@ -21,60 +20,32 @@ expect.addSnapshotSerializer({
   print: val => pretty(val)
 });
 
-function getSnapshotName(testName, snapshotTag: string) {
-  // TODO: If there are more the 1 snapshots (indicated by the number at the end) throw an exception
-  return `${testName}: ${snapshotTag} 1`;
-}
-
-function getSnapshotTag(
-  className: string,
-  methodName: string,
-  mockName: string
-) {
-  const snapshotNameTag = `[mockshot] [[${className} ${methodName} ${mockName}]]`;
-
-  return snapshotNameTag;
-}
-
-let commonSnapshotState;
-
-afterAll(async () => {
-  if (
-    commonSnapshotState &&
-    (commonSnapshotState.added > 0 || commonSnapshotState.updated > 0)
-  ) {
-    await generateMocks();
-  }
-});
-
 function toMatchMock(
-  received,
+  mock,
   className: string,
   methodName: string,
   mockName: string,
-  ignoredKeyPaths?: string[],
-  customMatcher?: any
+  ignoredKeyPaths?: string[]
 ) {
-  commonSnapshotState = this.snapshotState;
-
-  const snapshotTag = getSnapshotTag(className, methodName, mockName);
-  const snapshotName = getSnapshotName(this.currentTestName, snapshotTag);
+  const snapshotTag = `[mockshot] [[${className} ${methodName} ${mockName}]]`;
+  const snapshotName = `${this.currentTestName}: ${snapshotTag} 1`;
   const currentSnapshot = this.snapshotState._snapshotData[snapshotName];
 
   if (ignoredKeyPaths && currentSnapshot) {
     const parsedSnapshot = JSON.parse(currentSnapshot);
-    received = cloneDeep(received);
 
     ignoredKeyPaths.forEach(keyPath => {
       const val = get(parsedSnapshot.mock, keyPath);
-      const target = get(received, keyPath);
+      const target = get(mock, keyPath);
       if (val && target && typeof val === typeof target) {
-        set(received, keyPath, val);
+        set(mock, keyPath, val);
       }
     });
   }
-  const snapshot = { className, methodName, mockName, mock: received };
+
+  const snapshot = { className, methodName, mockName, mock };
   const snapshotNameTag = `[${className} ${methodName} ${mockName}]`;
+
   const result = expect(snapshot).toMatchSnapshot(
     `[mockshot] [${snapshotNameTag}]`
   );
---
+++ test/toMatchMock.spec.ts
@@ -0,0 +1,46 @@
+import "../src/matchers/toMatchMock";
+
+class HelloWorld {
+  bar(mock: "success"): any {
+    switch (mock) {
+      case "success":
+        return {
+          "data": {
+            "_id": "1234"
+          },
+          "id": "1234sddd56",
+          "some": "value"
+        }
+      default:
+        throw Error("Unknown mock: " + mock);
+    }
+  }
+
+  foo(mock: "success"): any {
+    switch (mock) {
+      case "success":
+        return {
+          "foo": "barr"
+        }
+      default:
+        return "unknown";
+    }
+  }
+}
+
+const hello = new HelloWorld();
+
+describe("toMatchMock", () => {
+  it("Should return correct schema", () => {
+    expect(hello.foo("success")).toMatchMock(HelloWorld.name, "foo", "success");
+  });
+
+  it("Should match a default parameter", () => {
+    expect(hello.foo("default-param")).toMatchMock(HelloWorld.name, "foo", "default-param");
+  });
+
+  it("Should match mock with ignoredKeyPaths", () => {
+    expect(hello.bar("success")).toMatchMock(HelloWorld.name, "bar", "success", ["id", "data._id"]);
+  });
+
+});
