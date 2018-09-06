9cbba308211e1c202ae1bc17df65a9ba239b76d0
idan.d <idandrd@gmail.com>
2018-09-06 12:01:18 +0300

    added classGenerator to the runner

--- src/run.ts
+++ src/run.ts
@@ -1,11 +1,11 @@
-import { ApiGenerator } from "./generators/ApiGenerator";
+import { ApiGenerator, ClassGenerator } from "./generators";
 import { GeneratorRunner } from "./GeneratorRunner";
 import { SnapshotFetcher } from "./SnapshotFetcher";
 
 export async function run(outputDir: string) {
   const snapshots = SnapshotFetcher.getSnapshots();
 
-  const generators = [new ApiGenerator()];
+  const generators = [new ApiGenerator(), new ClassGenerator()];
 
   const generatorRunner = new GeneratorRunner(outputDir, generators, snapshots);
   await generatorRunner.run();
