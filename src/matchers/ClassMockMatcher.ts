eccccadcb423fb4e0465828d9bc96fb2df62df46
Eli Shvartsman <eli@shvar.com>
2018-10-08 01:02:06 +0300

    ad a check for timestamp

--- src/matchers/ClassMockMatcher.ts
+++ src/matchers/ClassMockMatcher.ts
@@ -4,32 +4,34 @@ import { IClassSnapData } from "../contracts";
 export const ClassSnapshotTag = "[ClassSnap]";
 
 export function toMatchClassMock(
-  mock,
-  className: string,
-  methodName: string,
-  mockName: string,
-  ignoredKeyPaths?: string[]
+    mock,
+    className: string,
+    methodName: string,
+    mockName: string,
+    ignoredKeyPaths?: string[]
 ): { message(): string | (() => string); pass: boolean } {
-  const snapshotTag = `[mockshot] ${ClassSnapshotTag} [[${className} ${methodName} ${mockName}]]`;
-  const snapshotName = `${this.currentTestName}: ${snapshotTag} 1`;
-  const currentSnapshot = this.snapshotState._snapshotData[snapshotName];
+    const snapshotTag = `[mockshot] ${ClassSnapshotTag} [[${className} ${methodName} ${mockName}]]`;
+    const snapshotName = `${this.currentTestName}: ${snapshotTag} 1`;
+    const currentSnapshot = this.snapshotState._snapshotData[snapshotName];
 
-  if (ignoredKeyPaths && currentSnapshot) {
-    const parsedSnapshot = JSON.parse(currentSnapshot);
+    if (ignoredKeyPaths && currentSnapshot) {
+        const parsedSnapshot = JSON.parse(currentSnapshot);
 
-    ignoredKeyPaths.forEach(keyPath => {
-      const val = get(parsedSnapshot.mock, keyPath);
-      const target = get(mock, keyPath);
-      if (val && target && typeof val === typeof target) {
-        set(mock, keyPath, val);
-      }
-    });
-  }
+        ignoredKeyPaths.forEach(keyPath => {
+            const val = get(parsedSnapshot.mock, keyPath);
+            const target = get(mock, keyPath);
+            if (val && target && ((typeof val === typeof target) || isTimestamp(target))) {
+                set(mock, keyPath, val);
+            }
+        });
+    }
 
-  const snapshot: IClassSnapData = { className, methodName, mockName, mock };
+    const snapshot: IClassSnapData = { className, methodName, mockName, mock };
 
-  return {
-    pass: undefined === expect(snapshot).toMatchSnapshot(snapshotTag),
-    message: () => `expected ${snapshot} to match tag ${snapshotTag}`
-  };
+    return {
+        pass: undefined === expect(snapshot).toMatchSnapshot(snapshotTag),
+        message: () => `expected ${snapshot} to match tag ${snapshotTag}`
+    };
 }
+
+const isTimestamp = (target) => ("object" === typeof target) && (Date.parse(target))
--- test/matchers/toMatchClassMock.spec.ts
+++ test/matchers/toMatchClassMock.spec.ts
@@ -1,46 +1,47 @@
 import "../../src/matchers";
 
 class HelloWorld {
-  bar(mock: "success"): any {
-    switch (mock) {
-      case "success":
-        return {
-          "data": {
-            "_id": "1234"
-          },
-          "id": "1234sddd56",
-          "some": "value"
+    bar(mock: "success"): any {
+        switch (mock) {
+            case "success":
+                return {
+                    "data": {
+                        "_id": "1234"
+                    },
+                    "id": "1234sddd56",
+                    "some": "value",
+                    "timestamp": new Date()
+                }
+            default:
+                throw Error("Unknown mock: " + mock);
         }
-      default:
-        throw Error("Unknown mock: " + mock);
     }
-  }
 
-  foo(mock: "success"): any {
-    switch (mock) {
-      case "success":
-        return {
-          "foo": "barr"
+    foo(mock: "success"): any {
+        switch (mock) {
+            case "success":
+                return {
+                    "foo": "barr"
+                }
+            default:
+                return "unknown";
         }
-      default:
-        return "unknown";
     }
-  }
 }
 
 const hello = new HelloWorld();
 
 describe("toMatchMock", () => {
-  it("Should return correct schema", () => {
-    expect(hello.foo("success")).toMatchMock(HelloWorld.name, "foo", "success");
-  });
+    it("Should return correct schema", () => {
+        expect(hello.foo("success")).toMatchClassMock(HelloWorld.name, "foo", "success");
+    });
 
-  it("Should match a default parameter", () => {
-    expect(hello.foo("default-param")).toMatchMock(HelloWorld.name, "foo", "default-param");
-  });
+    it("Should match a default parameter", () => {
+        expect(hello.foo("default-param")).toMatchClassMock(HelloWorld.name, "foo", "default-param");
+    });
 
-  it("Should match mock with ignoredKeyPaths", () => {
-    expect(hello.bar("success")).toMatchMock(HelloWorld.name, "bar", "success", ["id", "data._id"]);
-  });
+    it("Should match mock with ignoredKeyPaths", () => {
+        expect(hello.bar("success")).toMatchClassMock(HelloWorld.name, "bar", "success", ["id", "data._id", "timestamp"]);
+    });
 
 });
