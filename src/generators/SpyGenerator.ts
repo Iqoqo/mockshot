00331adf58471dcb05c7e85d46fd1ade6854dd96
idan <idandrd@gmail.com>
2018-12-01 13:07:04 +0200

    wip

--- src/generators/SpyGenerator.ts
+++ src/generators/SpyGenerator.ts
@@ -1,18 +1,48 @@
+import { has, set } from "lodash";
+import { SourceFile } from "ts-simple-ast";
+import { IClassSnapData, IClassSnapshot, ISnapshot } from "../contracts";
+import { ClassSnapshotTag } from "../matchers/ClassMockMatcher";
+import { MockGenerator } from "./base";
+
+
+
+export class ClassSpyGenerator extends MockGenerator {
+  generate(
+    getFile: (filename: string) => SourceFile,
+    allSnapshots: ISnapshot[]
+  ) {
+    console.log("implement");
+  }
+
+  filterSnapshots(allSnapshots: ISnapshot[]): IClassSnapshot[] {
+    return allSnapshots.filter(snap => snap.key.includes(ClassSnapshotTag));
+  }
+
+}
+
 interface MockshotMock<P, T = {}> extends jest.Mock {
   give(mockName: P): jest.Mock<T>;
   giveOnce(mockName: P): jest.Mock<T>;
 }
 
 function getSpy<P extends string>(methodName) {
-  const newSpy = jest.fn() as MockshotMock<P>
-  newSpy.give = (mockName) => newSpy.mockImplementation(() => JobActionsServiceMocks[methodName](mockName))
-  newSpy.giveOnce = (mockName) => newSpy.mockImplementationOnce(() => JobActionsServiceMocks[methodName](mockName))
-  return newSpy
+  const newSpy = jest.fn() as MockshotMock<P>;
+  newSpy.give = mockName =>
+    newSpy.mockImplementation(() =>
+      JobActionsServiceMocks[methodName](mockName)
+    );
+  newSpy.giveOnce = mockName =>
+    newSpy.mockImplementationOnce(() =>
+      JobActionsServiceMocks[methodName](mockName)
+    );
+  return newSpy;
 }
 
-export const jobActionsServiceSpy: {duplicateJob: MockshotMock<"success" | "fail">} = {
+export const jobActionsServiceSpy: {
+  duplicateJob: MockshotMock<"success" | "fail">;
+} = {
   duplicateJob: getSpy("duplicateJob")
-}
+};
 
-jobActionsServiceSpy.duplicateJob.give("success")
-jobActionsServiceSpy.duplicateJob.giveOnce("fail")
+jobActionsServiceSpy.duplicateJob.give("success");
+jobActionsServiceSpy.duplicateJob.giveOnce("fail");
